c89607970a38b98bc1d817364f7d56c3906589d0
Mark Whitney <markwhitney@gmail.com>
2015-10-28 14:35:50 -0700

    whitespace and pep8

--- examples/doe.py
+++ examples/doe.py
@@ -3,8 +3,9 @@ import sys
 import time
 from rescale.client import RescaleFile, RescaleJob
 
+
 def main():
-    
+
     # get relative path to auxiliary files
     input_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                              'input')
@@ -13,8 +14,9 @@ def main():
     template_file = RescaleFile(file_path=os.path.join(input_dir, 'run_fmm'))
     input_files = []
     for file in ['hello.txt.zip', 'world.txt.zip']:
-        input_files.append( RescaleFile( file_path=os.path.join(input_dir, file) ) )
-    
+        input_files.append(RescaleFile(
+            file_path=os.path.join(input_dir, file)))
+
     # create parameter sweep dictionary
     job_data = {
         'name': 'DOE Test Job',
@@ -33,30 +35,30 @@ def main():
              'templateTasks': [
                 {'processedFilename': template_file.name,
                  'templateFile': {'id': template_file.id}}
-             ],
-             'inputFiles': [{'id': rescale_file.id} for rescale_file in input_files]
+            ],
+                'inputFiles': [{'id': rescale_file.id} for rescale_file in input_files]
             }
         ]
     }
-    
+
     # create parameter sweep job from dictionary
     job = RescaleJob(json_data=job_data)
-    
+
     # submit job
     job.submit()
-    
+
     # wait for job to complete
     job.wait()
 
     # download all files to a 'output' folder
     for file in job.get_files():
 
-        file_path = os.path.join('output',*file.path.split('/')[4:])
+        file_path = os.path.join('output', *file.path.split('/')[4:])
         file_dir = os.path.dirname(file_path)
-        
+
         if not os.path.isdir(file_dir):
             os.makedirs(file_dir)
-        
+
         file.download(target=file_path)
 
 if __name__ == '__main__':
--- rescale/client.py
+++ rescale/client.py
@@ -5,6 +5,7 @@ import os
 
 import requests
 
+
 class RescaleConnect(object):
     try:
         api_key = os.environ['RESCALE_API_KEY']
@@ -12,19 +13,21 @@ class RescaleConnect(object):
         api_key = None
     _root_url = 'https://platform.rescale.com/api/'
     _page_size = 100
-    
+
     def __repr__(self):
         return json.dumps(self._raw_data, sort_keys=True,
                           indent=4, separators=(',', ': '))
-    
+
     def _populate(self, json_data):
         self._raw_data = json_data
-        
+
         for attribute_name, attribute_data in json_data.items():
             setattr(self, attribute_name, attribute_data)
-    
-    def _paginate(self, url): # Looks like this will not work properly if people are deleting things.
-        response = self._request('GET', '{url}?page_size={page_size}'.format(url=url, page_size=self._page_size)).json()
+
+    # Looks like this will not work properly if people are deleting things.
+    def _paginate(self, url):
+        response = self._request('GET', '{url}?page_size={page_size}'.format(
+            url=url, page_size=self._page_size)).json()
         while True:
             for r in response['results']:
                 yield r
@@ -39,34 +42,37 @@ class RescaleConnect(object):
             headers['Content-Type'] = 'application/json'
 
         response = requests.request(method,
-                                    urllib.parse.urljoin(self._root_url, relative_url),
+                                    urllib.parse.urljoin(
+                                        self._root_url, relative_url),
                                     headers=headers,
                                     **kwargs)
         response.raise_for_status()
         return response
 
+
 class RescaleFile(RescaleConnect):
+
     def __init__(self, api_key=None, id=None, file_path=None, json_data=None):
         self.api_key  = api_key or RescaleConnect.api_key
         
         if id is not None:
             json_data = self._request('GET', 'files/{id}'.format(id=id)).json()
-        
+
         if file_path is not None:
             json_data = self._upload_file(file_path)
-        
+
         if json_data is not None:
             self._populate(json_data)
 
-
     def _upload_file(self, file_path):
-        # will fail on large files: http://docs.python-requests.org/en/latest/user/quickstart/#post-a-multipart-encoded-file
+        # will fail on large files:
+        # http://docs.python-requests.org/en/latest/user/quickstart/#post-a-multipart-encoded-file
         with open(file_path, 'rb') as fp:
             json_data = self._request('PUT',
                                       'files/contents/', files={'file': fp}).json()
             return json_data
-        
-    def download(self, target=None):  #TODO: can be async with a class counter
+
+    def download(self, target=None):  # TODO: can be async with a class counter
         response = self._request('GET', 'files/{file_id}/contents/'.format(file_id=self.id),
                                  stream=True)
         if not target:
@@ -77,32 +83,31 @@ class RescaleFile(RescaleConnect):
 
 
 class RescaleJob(RescaleConnect):
+
     def __init__(self, api_key=None, id=None, json_data=None):
-        self.api_key  = api_key or RescaleConnect.api_key
-        
+        self.api_key = api_key or RescaleConnect.api_key
+
         if id is not None:
-            self._populate(self._request('GET', 'jobs/{id}'.format(id=id)).json())
-            
+            self._populate(self._request(
+                'GET', 'jobs/{id}'.format(id=id)).json())
+
         if json_data is not None:
             self._populate(self._request('POST',
-                                      'jobs/', data=json.dumps(json_data)).json())
-    
+                                         'jobs/', data=json.dumps(json_data)).json())
+
     def get_statuses(self):
         return self._paginate('jobs/{job_id}/statuses/'.format(job_id=self.id))
-    
+
     def get_latest_status(self):
         return next(self.get_statuses(), None)
 
     def get_files(self):
         for json_data in self._paginate('jobs/{job_id}/files/'.format(job_id=self.id)):
-            yield RescaleFile(self.api_key, json_data=json_data)        
+            yield RescaleFile(self.api_key, json_data=json_data)
 
     def submit(self):
         return self._request('POST', 'jobs/{job_id}/submit/'.format(job_id=self.id))
-    
+
     def wait(self, refresh_rate=60):
         while not self.get_latest_status()['status'] == 'Completed':
             time.sleep(refresh_rate)
-
-    
-    
